51c913c0a29562f61e16d6792f878f6744a14fbe
MishraTanishq619 <mishratanishq619@gmail.com>
2026-06-12 15:12:42 +0530

    registration_popups: altered for older states

--- src/components/registration-popups/ExistingUserContent.tsx
+++ src/components/registration-popups/ExistingUserContent.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { ChildPopupProps } from './types';
 import AlreadyRegisteredContent from './AlreadyRegisteredContent';
 import SelfReferralContent from './SelfReferralContent';
+import OldUserContent from './OldUserContent';
+
+const OLD_USER_STATUSES = ['free_completed_recent', 'paid_user', 'subscription_expired_recently'];
 
 const ExistingUserContent: React.FC<ChildPopupProps> = (props) => {
     if (props.status === 'self_referral') {
         return <SelfReferralContent {...props} />;
+    }
+    if (props.status === 'already_registered' || props.status === 'free_ongoing') {
+        return <AlreadyRegisteredContent {...props} />;
+    }
+    if (OLD_USER_STATUSES.includes(props.status)) {
+        return <OldUserContent {...props} />;
     }
     return <AlreadyRegisteredContent {...props} />;
 };
